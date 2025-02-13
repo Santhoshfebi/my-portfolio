f11cf0d66181fb983997e6135a5f658d899f65c9
Santhoshfebi <santhoshrajr121@gmail.com>
2025-02-13 21:24:54 +0530

    changes in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-      <nav className="bg-white text-white mt-2 p-4 flex justify-end">
+      <nav className="bg-white text-black p-4 flex justify-end">
         <ul className="flex space-x-16">
           <li><a href="#about" className="hover:text-white-400">About Me</a></li>
           <li><a href="#projects" className="hover:text-white-400">Projects</a></li>
