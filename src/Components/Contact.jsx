ff7ae8817562b7178122de24e1a7aaa14cf7af72
Santhoshfebi <santhoshrajr121@gmail.com>
2025-02-13 21:40:17 +0530

    alignment changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-      <section id="contact" className="p-10 bg-gray-900 text-white text-center">
+      <section id="contact" className="p-10 bg-gray-900 text-white text-center h-screen flex flex-col justify-center gap-5">
         <h2 className="text-3xl font-bold">Contact Me</h2>
         <p>Email: santhoshrajr121@gmail</p>
         <p>LinkedIn: https://www.linkedin.com/in/santhosh-raj-28b8281aa/</p>
