638eec294071e3505a6fd4d0cc675330f0c77ac1
Santhoshfebi <santhoshrajr121@gmail.com>
2025-04-30 10:30:34 +0530

    update in projects

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
       <section id="contact" className="p-10 bg-cyan-900 text-white text-center flex flex-col justify-center gap-5">
         <h2 className="text-3xl font-bold">Contact Me</h2>
-        <p>Email: santhoshrajr121@gmail</p>
+        <p>Email: santhoshrajr121@gmail.com</p>
         <p>LinkedIn: https://www.linkedin.com/in/santhosh-raj-28b8281aa/</p>
         <div className="btn flex justify-center gap-8">
             <button className="h-10 w-10  hover:bg-white"><a href="https://www.linkedin.com/in/santhosh-raj-28b8281aa/" target="_blank"><img src={linkedin} alt="" /></a>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,27 +1,39 @@
 const Projects = () => {
-    return (
-      <section id="projects" className="p-10 h-screen bg-gray-100 flex flex-col justify-evenly">
-        <h2 className="text-3xl font-bold text-center">Projects</h2>
-        <div className="grid md:grid-cols-2 gap-6 mt-6">
-          <div className="bg-white p-4 shadow rounded-lg">
-            <h3 className="text-xl font-semibold">Bookmyshow Clone Website</h3>
-            <p className="text-gray-600">A cloned website with basic user interactions using HTML,CSS,JavaScript,ReactJS and MySQL.</p>
-            <p className="text-gray-600"><strong>Code Base Link:</strong>  <a href="https://github.com/Santhoshfebi/moviebooking">github/booking_app</a></p>
-            <p className="text-gray-600"><strong>Deployment Link:</strong> <a href="https://moviebooking-git-master-santhoshs-projects-40d568cb.vercel.app/">Booking APP</a></p>
-          </div>
-          <div className="bg-white p-4 shadow rounded-lg">
-            <h3 className="text-xl font-semibold">Quizz App</h3>
-            <p className="text-gray-600">A simple Quizz App where you can attend the quizz. we have features like scoreboard,  correct answers and worng answers.</p>
-            <p className="text-gray-600"><strong>Code Base Link:</strong>  <a href="https://github.com/Santhoshfebi/Quizz" target="_blank">github/Quizz_app</a></p>
-            <p className="text-gray-600"><strong>Deployment Link:</strong>  <a href="https://quizz-react-app-theta.vercel.app">Quizz APP</a></p>
-          </div>
-          {/* <div className="bg-white p-4 shadow rounded-lg">
-            <h3 className="text-xl font-semibold">simple Quizz App</h3>
-            <p className="text-gray-600">Description of Project 2.</p>
-          </div> */}
+  return (
+    <section id="projects" className="p-10 h-screen bg-gray-100 flex flex-col justify-evenly">
+      <h2 className="text-3xl font-bold text-center">Projects</h2>
+      <div className="grid md:grid-cols-2 gap-6 mt-6">
+        <div className="bg-white p-4 shadow rounded-lg">
+          <h3 className="text-xl font-semibold">Bookmyshow Clone Website</h3>
+          <p className="text-gray-600">The BookMyShow Clone is a fully responsive, interactive web application built using ReactJS along with HTML, CSS, and JavaScript, designed to replicate the core functionality and user experience of the original BookMyShow platform. 
+            This project uses API to fetch dynamic data for movies, including posters, descriptions, genres, ratings, and showtimes from the database.</p>
+          <p className="text-gray-600"><strong>Code Base Link:</strong>  <a href="https://github.com/Santhoshfebi/moviebooking">github/booking_app</a></p>
+          <p className="text-gray-600"><strong>Deployment Link:</strong> <a href="https://moviebooking-git-master-santhoshs-projects-40d568cb.vercel.app/">Booking APP</a></p>
         </div>
-      </section>
-    );
-  };
-  export default Projects;
-  
+        <div className="bg-white p-4 shadow rounded-lg">
+          <h3 className="text-xl font-semibold">Quizz App</h3>
+          <p className="text-gray-600">The app features a homepage with Start selection, a question interface that displays one question at a time with multiple-choice answers,
+             and a results summarizing the user’s score. Built with a focus on usability and responsiveness, the app supports features such as instant feedback, and optionally a timer for each question.
+            The front end is developed using HTML, CSS, and JavaScript</p>
+          <p className="text-gray-600"><strong>Code Base Link:</strong>  <a href="https://github.com/Santhoshfebi/Quizz" target="_blank">github/Quizz_app</a></p>
+          <p className="text-gray-600"><strong>Deployment Link:</strong>  <a href="https://quizz-react-app-theta.vercel.app">Quizz APP</a></p>
+        </div>
+        <div className="bg-white p-4 shadow rounded-lg">
+          <h3 className="text-xl font-semibold">Login Page</h3>
+          <p className="text-gray-600">The login page is a user interface that allows registered users to securely access their personal accounts within the system. It typically contains input fields for a username or email address and password,
+            along with action buttons such as "Log In", "Forgot Password?", and optionally "Sign Up" for new users.Security measures like input validation, password masking, and error feedback</p>
+          <p className="text-gray-600"><strong>Code Base Link:</strong>  <a href="https://github.com/Santhoshfebi/login_page" target="_blank">github/login_page</a></p>
+          <p className="text-gray-600"><strong>Deployment Link:</strong>  <a href="https://login-page-iota-vert.vercel.app/">Login Page</a></p>
+        </div>
+        <div className="bg-white p-4 shadow rounded-lg">
+          <h3 className="text-xl font-semibold">Airbnb Clone Website</h3>
+          <p className="text-gray-600">This project is a front-end clone of the Airbnb website, built using HTML and CSS to replicate the core visual layout and styling of the original platform. The website features a homepage with a navigation bar, a search bar, featured listings with images and property details, and a footer with relevant links. Each listing card displays key information like the property name, image, price per night, location, and rating.
+             The design is responsive, ensuring optimal viewing on desktops, tablets, and mobile devices.</p>
+          <p className="text-gray-600"><strong>Code Base Link:</strong>  <a href="https://github.com/Santhoshfebi/airbnb-clone" target="_blank">github/airbnb</a></p>
+          <p className="text-gray-600"><strong>Deployment Link:</strong>  <a href="https://airbnb-clone-rho-six.vercel.app/">Airbnb</a></p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Projects;
