67dc9345e8ad6401930f9b13ae5180de5345a3a8
Santhoshfebi <santhoshrajr121@gmail.com>
2025-10-24 11:52:03 +0530

    frame motion

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+
 
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState("");
